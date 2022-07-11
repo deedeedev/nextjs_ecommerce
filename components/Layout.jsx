ee053b2f291f4ade0d449706d29b6ed72108299b
Davide <myearthlydelight@gmail.com>
2022-07-11 15:04:47 +0200

    added Layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Head from "next/head";
 
-const Layout = () => {
-    return <div>Layout</div>;
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Layout = ({ children }) => {
+    return (
+        <div className="layout">
+            <Head>
+                <title>JS Mastery Store</title>
+            </Head>
+            <header>
+                <Navbar />
+            </header>
+            <main className="main-container">{children}</main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    );
 };
 
 export default Layout;
