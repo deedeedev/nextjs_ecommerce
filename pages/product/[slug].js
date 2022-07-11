fc376f47dba40b4cd17031f6119fb5fd06018195
Davide <myearthlydelight@gmail.com>
2022-07-11 21:01:50 +0200

    ProductDetails

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,16 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from "react-icons/ai";
 
+import { Product } from "../../components";
 import { client, urlFor } from "../../lib/client";
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
 
     return (
         <div>
             <div className="product-detail-container">
                 <div>
                     <div className="image-container">
-                        <img src={urlFor(image && image[0])} />
+                        <img className="product-detail-image" src={urlFor(image && image[index])} />
+                    </div>
+                    <div className="small-images-container">
+                        {image?.map((item, i) => (
+                            <img
+                                className={i === index ? "small-image selected-image" : "small-image"}
+                                src={urlFor(item)}
+                                onMouseEnter={() => setIndex(i)}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="product-detail-desc">
+                    <h1>{name}</h1>
+                    <div className="reviews">
+                        <div>
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiOutlineStar />
+                        </div>
+                        <p>(20)</p>
+                    </div>
+                    <h4>Details: </h4>
+                    <p>{details}</p>
+                    <p className="price">${price}</p>
+                    <div className="quantity">
+                        <h3>Quantity:</h3>
+                        <p className="quantity-desc">
+                            <span className="minus" onClick="">
+                                <AiOutlineMinus />
+                            </span>
+                            <span className="num" onClick="">
+                                0
+                            </span>
+                            <span className="plus" onClick="">
+                                <AiOutlinePlus />
+                            </span>
+                        </p>
+                    </div>
+                    <div className="buttons">
+                        <button className="add-to-cart" type="button" onClick="">
+                            Add to Cart
+                        </button>
+                        <button className="buy-now" type="button" onClick="">
+                            Buy Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="maylike-products-wrapper">
+                <h2>You may also like</h2>
+                <div className="marquee">
+                    <div className="maylike-products-container track">
+                        {products.map((item) => (
+                            <Product key={item._id} product={item} />
+                        ))}
                     </div>
                 </div>
             </div>
