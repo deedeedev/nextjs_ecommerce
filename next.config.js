bb415afb10059414092fea18cf41dad644e8e679
Davide <myearthlydelight@gmail.com>
2022-07-13 20:17:42 +0200

    added Content Security Policy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    async headers() {
+        const ContentSecurityPolicy = `
+            style-src 'unsafe-inline';
+        `;
+        return [
+            {
+                source: "/",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        // value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+                        value: "",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
